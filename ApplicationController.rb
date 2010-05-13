81394379ea784a10161a003d70b7036a081b2cfe
Patrick Huesler <patrick.huesler@gmail.com>
2010-05-13 18:33:09 +0200

    Positioning of label

--- ApplicationController.rb
+++ ApplicationController.rb
@@ -4,10 +4,12 @@
 # Created by phuesler on 13.05.10.
 # Copyright 2010 huesler informatik. All rights reserved.
 class ApplicationController
-  attr_accessor :contentView
+  attr_accessor :contentView, :nameLabel
   
   def applicationDidFinishLaunching(sender)
     NSLog("Launched app")
+    contentView.layer.backgroundColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+    # CGColorRelease(contentView.layer.backgroundColor)
   end
   
   def toggleFullscreen(sender)
@@ -15,10 +17,10 @@ class ApplicationController
       contentView.exitFullScreenModeWithOptions(nil)
     else
       contentView.enterFullScreenMode(contentView.window.screen,withOptions:nil)
-      contentView.subviews.each do |subview|
-        subview.removeFromSuperview
-        contentView.addSubview(subview)
-      end
+      # contentView.subviews.each do |subview|
+      #   subview.removeFromSuperview
+      #   contentView.addSubview(subview)
+      # end
     end
   end
 end
